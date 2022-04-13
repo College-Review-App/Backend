cccefb46766e650ae99ac41eb33719f43dda949d
ethan1234k <ethanwoodhill@gmail.com>
2022-04-13 15:12:13 -0700

    Added SQL Driver to connect to MySQL database.

--- src/main/kotlin/college/app/backend/BackendApplication.kt
+++ src/main/kotlin/college/app/backend/BackendApplication.kt
@@ -1,18 +1,31 @@
 package college.app.backend
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
+
 
 // TODO: Add Database
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
-class BackendApplication
+@SpringBootApplication
+class BackendApplication(@Autowired val jdbcTemplate: JdbcTemplate) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        var colleges =  jdbcTemplate.query(
+                "SELECT * FROM CollegeData"
+        ) { rs, rowNum ->
+            College(rs.getString("id"),
+                    rs.getString("college_name"),
+                    rs.getDouble("GPA"))
+        }
+        println(colleges)
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<BackendApplication>(*args)
+    runApplication<BackendApplication>(*args)
 }
 
 // https://medium.com/@tcbasche/cors-in-spring-boot-with-kotlin-55eb5385f0e
@@ -20,13 +33,29 @@ fun main(args: Array<String>) {
 // Temp Solution: Define allowed origins
 
 @RestController
-class MessageResource {
-	@CrossOrigin(origins = ["http://localhost:8080", "http://localhost:3000"])
-	@GetMapping
-	fun index(): List<Message> = listOf(
-			Message("1", "Hello"),
-			Message("2", "Goodbye")
-	)
+class CollegeResource {
+    @CrossOrigin(origins = ["http://localhost:8080", "http://localhost:3000"])
+    @GetMapping
+    fun index(): List<College> = listOf(
+            College("1", "Hello", 4.00),
+            College("2", "Goodbye", 3.98)
+    )
 }
 
-data class Message(val id: String, val text: String)
+//@RequestMapping(path = ["/blogs"])
+//@RestController
+//class GetCollegeData(@Autowired val jdbcTemplate: JdbcTemplate) {
+//    @GetMapping
+//    fun index() {
+//        var colleges =  jdbcTemplate.query(
+//                "SELECT * FROM CollegeData"
+//        ) { rs, rowNum ->
+//            College(rs.getString("id"),
+//                    rs.getString("college_name"),
+//                    rs.getDouble("GPA"))
+//        }
+//        println(colleges)
+//    }
+//}
+
+data class College(val id: String, val college_name: String, val GPA: Double)
